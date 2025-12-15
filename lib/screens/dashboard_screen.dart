f75f1c07191ca860418c04ba401640a7999b47ff
Shreejal Shrestha <shresthashreejal84@gmail.com>
2025-12-15 12:17:23 +0545

    dashboard done

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,20 +1,57 @@
+import 'package:fashion_store_trendora/screens/bottom_screen/about_screen.dart';
+import 'package:fashion_store_trendora/screens/bottom_screen/cart_screen.dart';
+import 'package:fashion_store_trendora/screens/bottom_screen/profile_screen.dart';
+import 'package:fashion_store_trendora/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const HomeScreen(),
+    const CartScreen(),
+    const ProfileScreen(),
+    const AboutScreen(),
+  ]; //  <-- Missing semicolon FIXED!
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard'),
-        backgroundColor: Colors.redAccent,
+        title: const Text('This is dashboard'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
-        child: Text(
-          'Welcome to Dashboard',
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-        ),
+
+      body: lstBottomScreen[_selectedIndex], // <-- Shows the correct page
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'About'),
+        ],
+        backgroundColor: Colors.red,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
